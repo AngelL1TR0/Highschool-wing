5e61f09f5b7eae725f8f9c56d9b7284695cf6f48
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-11-08 12:32:07 +0100

    first commit

---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,18 @@
+package entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Student {
+
+    private String nif;
+    private String name;
+    private String surname;
+    private int zipCode;
+}
