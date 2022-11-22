168f657f1ee45fb9a3bf4f043ed3bed514568b8b
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-11-22 12:31:04 +0100

    sixth commit

--- src/main/java/forms/HighschoolMainForm.java
+++ src/main/java/forms/HighschoolMainForm.java
@@ -15,14 +15,18 @@ public class HighschoolMainForm {
     private JButton showStudentsButton;
     private JButton showGroupsButton;
     private JButton addStudentQuickAction;
-    private JPanel menusPanel;
     private JButton addGroupQuickAction;
 
-    public HighschoolMainForm() {
+    public HighschoolMainForm(JFrame frame) {
+        frame.setContentPane(mainPanel);
+        createMenuBar(frame);
+
         showStudensForm();
         showStudentsButton.addActionListener(e -> {
             showStudensForm();
         });
+
+
         showGroupsButton.addActionListener(e -> {
             showGroupsForm();
         });
@@ -40,6 +44,35 @@ public class HighschoolMainForm {
                 dialog.setVisible(true);
             }
         });
+    }
+
+    private void createMenuBar(JFrame frame) {
+        ImageIcon image1 = new ImageIcon("src/main/resources/java.png");
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu1 = new JMenu("Menú1");
+        JMenu menu2 = new JMenu("Menú2");
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+        JMenuItem item11 = new JMenuItem("Menu item with Plain Text");
+        JMenuItem item12 = new JMenuItem("Menu item with Text & Image");
+        JMenuItem item13 = new JMenuItem("", image1);
+
+        JRadioButtonMenuItem item21 =  new JRadioButtonMenuItem("Menu Item with Radio Button");
+        JRadioButtonMenuItem item22 =  new JRadioButtonMenuItem("Menu Item 2 with Radio Button");
+
+        JCheckBoxMenuItem item31 = new JCheckBoxMenuItem("Menu Item with Check box");
+        JCheckBoxMenuItem item32 = new JCheckBoxMenuItem("Menu Item 2 with Check box");
+
+        menu1.add(item11);
+        menu1.add(item12);
+        menu1.add(item13);
+        menu1.add(item21);
+        menu1.add(item22);
+        menu1.add(item31);
+        menu1.add(item32);
+
+        frame = (JFrame) SwingUtilities.windowForComponent(mainPanel);
+        frame.setJMenuBar(menuBar);
     }
 
     private void showGroupsForm() {
@@ -69,7 +102,7 @@ public class HighschoolMainForm {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setVisible(true);
-        var form = new HighschoolMainForm();
+        var form = new HighschoolMainForm(frame);
         frame.setContentPane(form.mainPanel);
 
         frame.pack();
